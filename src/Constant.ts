6affe6cecf4be9d7a01d02262cbe5673d144a991
jcastanodev <jcastanodev@gmail.com>
2026-06-09 17:09:03 -0500

    change number

--- src/Constant.ts
+++ src/Constant.ts
@@ -2,10 +2,10 @@ export const DEFAULT_URL = 'http://localhost:5173'
 export const MINIDISCOS_PAGE_URL = 'https://desarrollodesoftwarepereira.github.io'
 
 // Base price includes disc + transparent holder
-export const BASE_PRICE_COP = 35_000
+export const BASE_PRICE_COP = 30_000
 export const NFC_PRICE_COP = 20_000
-export const HOLDER_COLOR_PRICE_COP = 15_000
+export const HOLDER_COLOR_PRICE_COP = 10_000
 
-export const WHATSAPP_NUMBER = '573XXXXXXXXX'
+export const WHATSAPP_NUMBER = '573128819198'
 export const WHATSAPP_MESSAGE_TEMPLATE = (pdfName: string) =>
   `Hola! Quiero ordenar un MiniDisco personalizado. Adjunto el PDF con las especificaciones: ${pdfName}`
